be0baab1090c0ddd10f3b0a894bc4e7d4f5e6c4d
broering <victorbroerings@gmail.com>
2026-04-15 09:51:31 -0300

    feat<api/dependencies/user, routers/v1/users/create>

---
+++ api/app/api/dependecies/user.py
@@ -0,0 +1,12 @@
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.application.use_cases.user.create import CreateUser
+from app.infrastructure.repositories.user_repository import UserRepository
+from app.infrastructure.session import get_session
+
+
+def get_create_user_use_case(
+    session: AsyncSession = Depends(get_session),
+) -> CreateUser:
+    return CreateUser(repository=UserRepository(session))
