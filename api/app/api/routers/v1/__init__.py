fb0d79048c99197fefcc5b0bb727d59dadb5f99e
broering <victorbroerings@gmail.com>
2026-04-15 10:25:47 -0300

    feat<main> add router

---
+++ api/app/api/routers/v1/__init__.py
@@ -0,0 +1,8 @@
+from fastapi import APIRouter
+
+from .user_router import router as user_router
+
+api_router = APIRouter()
+api_router.include_router(user_router)
+
+__all__ = ["api_router"]
--- api/main.py
+++ api/main.py
@@ -1,15 +1,16 @@
 from fastapi import FastAPI
 from fastapi.exceptions import RequestValidationError
 from pydantic import ValidationError
-
 from app.api.config.scalar_config import router as scalar_router
 from app.api.errors.handlers import validation_error_handler, request_validation_error_handler
+from app.api.routers.v1 import api_router as v1_router
 from app.infrastructure.config.settings import get_settings
 
 get_settings()
 
 app = FastAPI()
 app.include_router(scalar_router)
+app.include_router(v1_router, prefix="/api/v1")
 
 app.add_exception_handler(ValidationError, validation_error_handler)
 app.add_exception_handler(RequestValidationError, request_validation_error_handler)
