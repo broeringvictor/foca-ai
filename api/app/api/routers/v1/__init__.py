6f78bc7fa7a27b2082fefec1fe855c8e692aacb9
broering <victorbroerings@gmail.com>
2026-04-17 09:01:35 -0300

    feat<exam, question>: implement CRUD operations and endpoints for exams and questions

--- api/app/api/routers/v1/__init__.py
+++ api/app/api/routers/v1/__init__.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
 from .auth_router import router as auth_router
+from .exam_router import router as exam_router
+from .question_router import router as question_router
 from .study_note_router import router as study_note_router
 from .user_router import router as user_router
 
@@ -8,5 +10,7 @@ api_router = APIRouter()
 api_router.include_router(auth_router)
 api_router.include_router(user_router)
 api_router.include_router(study_note_router)
+api_router.include_router(exam_router)
+api_router.include_router(question_router)
 
 __all__ = ["api_router"]
